79fd5159b9dcaaea0064bcb94eb4a273338172ec
lcevallos <luispaulcevallos@gmail.com>
2026-01-31 03:47:10 -0500

    fix nueva contraseña como opcional

--- Models/Request/UsuarioRequest.cs
+++ Models/Request/UsuarioRequest.cs
@@ -15,7 +15,7 @@ namespace Models.Request
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
-        public string NuevaContrasena { get; set; } = string.Empty;
+        public string? NuevaContrasena { get; set; } = string.Empty;
         public string Nombre { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public bool Activo { get; set; } = true;
